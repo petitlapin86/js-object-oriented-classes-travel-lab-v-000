fa82ad061b15873a9394185fac4cb74171adef68
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-14 14:34:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ this.name = name;
 this.startDate = new Date (startDate)
   }
 
-yearsExperienceFromBeginningOf() {
-  let endDate = new Date(year, 1, 1)
+  yearsExperienceFromBeginningOf(year){
+    let endDate = new Date(year, 1, 1)
     let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
     return parseInt(totalYears)
-} // closes yearsExperienceFromBeginningOf
+  } // closes yearsExperienceFromBeginningOf
+
 } //closes driver
 
 
@@ -19,19 +20,21 @@ class Route { //opens route
     this.endingLocation = endingLocation
   }
   avenueToInteger(avenue){
-    return eastWest.indexOf(avenue)}
+    return eastWest.indexOf(avenue)} //returns avenue as a number. 
 
 blocksTravelled(){
   let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
      let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
      return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
 } // closes blocksTravelled
+
 estimatedTime(peak){
   if(peak){
     return this.blocksTravelled()/2 // estimates time in minutes, with travel time of 2 blocks in a minute
   } else {
     return this.blocksTravelled()/3} // estimates time in minutes, with travel time of 3 blocks in a minute
 } // closes estimatedTime
+
 } //closes route
 
 let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
